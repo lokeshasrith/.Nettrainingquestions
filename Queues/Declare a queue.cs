3b2691d784f77d63b70a9088a52cc126b10a28fb
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:48:29 +0530

    Create Declare a queue.cs

---
+++ Queues/Declare a queue.cs
@@ -0,0 +1,11 @@
+// create a new queue of type int, named primes.
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Queue<int> names = new Queue<int>(); 
+    }
+}
