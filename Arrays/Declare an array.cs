40cd160e21504809977d025f47d2563c8b1ee0b4
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:08:03 +0530

    Create Declare an array.cs

---
+++ Arrays/Declare an array.cs
@@ -0,0 +1,9 @@
+// Creating a new string array, without using the var keyword. This array must be of length 0.
+using System;
+public class Declareanarray
+{    
+  public static void Main()    
+  {       	
+    string[] emptyArray = new string[0];    // array
+  }
+}
