e6cfea91e3e96066316e97df57499a5df001dd5e
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 11:19:12 +0530

    Create Declare a stack.cs

---
+++ Stacks/Declare a stack.cs
@@ -0,0 +1,11 @@
+// create a new stack of type string, named films.
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Stack<string> names = new Stack<string>();
+    }
+}
