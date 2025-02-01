6870cb420a67b5a4fadb509ecc7f518a63ffa505
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:12:51 +0530

    Create Read and write array values.cs

---
+++ Arrays/Read and write array values.cs
@@ -0,0 +1,12 @@
+// set the first item in the array to 'Han Solo'. On the next line, write the first item in the array to the Console.
+using System;
+
+public class Readandwritearrayvalues
+{
+    public static void Main()
+    {
+        string[] names = new string[1];
+    		names[0] = "Han Solo";
+    		Console.WriteLine(names[0]);
+    }
+}
