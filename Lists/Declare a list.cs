f62ba971f3602bb088d532b8a4a51fcedbf48269
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:24:35 +0530

    Create Declare a list.cs

---
+++ Lists/Declare a list.cs
@@ -0,0 +1,11 @@
+// creating a new string list, without using the var keyword. This list should contain no values.
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+		List<string> names = new List<string>();
+    }
+}
