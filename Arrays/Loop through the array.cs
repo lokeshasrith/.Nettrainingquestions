2fc7df5b55cd0dd313d807626b6dcf4adcf321ce
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:18:15 +0530

    Create Loop through the array.cs

---
+++ Arrays/Loop through the array.cs
@@ -0,0 +1,17 @@
+// create a for loop after the array declaration, and loop through the values, writing each to the console.
+
+using System;
+
+using System;
+
+public class Loopthroughthearray
+{
+    public static void Main()
+    {
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for(int i = 1;i<=numbers.Length;i++)
+    		{
+    			Console.WriteLine(i);
+    		}
+    }
+}
