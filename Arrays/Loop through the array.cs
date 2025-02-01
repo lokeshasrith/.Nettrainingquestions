00e270ef470301975dcb821b073bfa960a19e89c
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:21:13 +0530

    Update Loop through the array.cs

--- Arrays/Loop through the array.cs
+++ Arrays/Loop through the array.cs
@@ -9,9 +9,9 @@ public class Loopthroughthearray
     public static void Main()
     {
         int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        for(int i = 1;i<=numbers.Length;i++)
-    		{
-    			Console.WriteLine(i);
-    		}
+        for(int i = 0;i < numbers.Length;i++)
+		{
+			Console.WriteLine(numbers[i]);
+		}
     }
 }
