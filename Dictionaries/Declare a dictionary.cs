73bb98b9da12965f896b4be6ba0e6997965fee94
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 11:04:03 +0530

    Create Declare a dictionary.cs

---
+++ Dictionaries/Declare a dictionary.cs
@@ -0,0 +1,11 @@
+// create a new dictionary, where the key type is a string and the value type is an int. This dictionary should contain no values.
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, int> people = new Dictionary<string, int>();
+    }
+}
