aa3675b4f1754fafbf24d517eb6993837109e28b
asrith862002 <129488955+lokeshasrith@users.noreply.github.com>
2025-02-01 10:38:56 +0530

    Create Remove an item from the dictionary.cs

---
+++ Lists/Remove an item from the dictionary.cs
@@ -0,0 +1,18 @@
+// remove the item with the key 'Han' from the dictionary.
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, bool> characters = new Dictionary<string, bool>()
+        {
+            { "Luke", true },
+            { "Han", false },
+            { "Chewbacca", false }
+        };
+
+        characters.Remove("Han");  // remove
+    }
+}
